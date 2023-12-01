e529cdeedd9403c9e58ea019218ece49ec994466
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-12-01 10:53:49 +0500

    add array types

--- index.ts
+++ index.ts
@@ -59,3 +59,36 @@ function ReturnNot(params: number = 5): number {
 const resp = ReturnNot();
 
 console.log(resp);
+
+const arrfunc = (arg: number = 4): number => arg * 4;
+console.log(arrfunc());
+
+// void in typescript
+//a function that not return a value
+
+const returnvoid = (argument: number): void => {
+  console.log(argument);
+};
+
+console.log(returnvoid(4));
+
+// Never type not return anything  or that variable can never have a value
+//it can help catch errors at compile-time instead of run-time
+// a varibale that can never have an value
+
+//a function that always throws an error
+
+function throwError(msg: string): never {
+  throw new Error(msg);
+}
+
+function throwNever(msg: string): never {
+  throw new Error(msg);
+}
+console.log(throwNever("hi developer"));
+
+// Array Types
+
+//array are the type of object that can stor multiple value
+
+const arr2: (number | string | boolean)[] = [3, 4, 5, 6, "dev", false];
