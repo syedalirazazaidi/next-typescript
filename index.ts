e7db987683bc5dc2fdaf75947de01c42b5d7f3d6
DESKTOP-7V9Q99B\AKU <akuraza6atgmail.com>
2023-12-01 12:12:59 +0500

    intersection type in typescript

--- index.ts
+++ index.ts
@@ -139,3 +139,30 @@ function objName({
 
 const respt = objName({ name: "aliraza", numb: 50 });
 console.log(respt);
+
+// Intersection Type
+
+interface BusinessPartner {
+  name: string;
+  credit: number;
+}
+
+interface Identity {
+  id: number;
+  name: string;
+}
+
+interface Contact {
+  email: string;
+  phone: string;
+  add?: string;
+}
+
+type Employee = Identity & Contact;
+
+let e: Employee = {
+  id: 100,
+  name: "John Doe",
+  email: "john.doe@example.com",
+  phone: "(408)-897-5684",
+};
